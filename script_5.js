8ef336db25aa2f1d0e9110d54aa6df5a576f75a8
agnesarth <agnesarth@hotmail.fr>
2020-05-21 17:47:32 +0200

    Ex 5 done

--- script_5.js
+++ script_5.js
@@ -13,17 +13,80 @@ const books = [
   { title: 'Guerre et Paix', id: 748147, rented: 19 }
 ];
 
-/*
-for (let {rented: r} of books) {
-  if (r == 0);
-    console.log(`Au moins un livre n'a jamais été emprunté`);
-  elsif (r >= 1);
-  console.log(`Tous les livres ont été empruntés au moins une fois`)
-} */
 
-const booksrented = books.filter(e => e.rented = 0)
-if booksrented == nil
-  console.log(`Tous les livres ont été empruntés au moins une fois`)
-else
-  console.log(`Un livre n'a jamais été emprunté`)
-end
+//Question 1:
+if(books.some(book => book.rented === 0)){
+  console.log("Un livre n'a jamais été emprunté.");
+} else{
+  console.log("Tous les livres ont été empruntés au moins une fois.");
+}
+
+//Question 2:
+function compare1(a, b) {
+  const rentedA = a.rented
+  const rentedB = b.rented
+
+let comparison = 0;
+  if (rentedA > rentedB) {
+    comparison = 1;
+  } else if (rentedA < rentedB) {
+    comparison = -1;
+  }
+  return comparison * -1;
+}
+
+order = books.sort(compare1);
+console.log(`Le livre le plus emprunté est :`)
+console.log(order[0])
+
+
+//Question 3:
+function compare(a, b) {
+  const rentedA = a.rented
+  const rentedB = b.rented
+
+let comparison = 0;
+  if (rentedA > rentedB) {
+    comparison = 1;
+  } else if (rentedA < rentedB) {
+    comparison = -1;
+  }
+  return comparison;
+}
+
+order = books.sort(compare);
+console.log(`Le livre le moins emprunté est :`)
+console.log(order[0])
+
+
+//Question 4:
+const book1 = books.filter(e => e.id === 873495 )
+console.log(`Voici le livre avec l'id 873485`)
+console.log(book1)
+
+//Question 5:
+for(let n in books) {
+  if(books[n].id === 133712){
+    delete books[n];
+  }
+}
+console.log(`Le livre avec l'id 133712 a été supprimé`)
+console.log(books)
+
+//Question 6:
+console.log(`Voici la liste classée par ordre alphabétique :`)
+function compare(a, b) {
+  const nameA = a.title.toUpperCase();
+  const nameB = b.title.toUpperCase();
+
+let comparison = 0;
+  if (nameA > nameB) {
+    comparison = 1;
+  } else if (nameA < nameB) {
+    comparison = -1;
+  }
+  return comparison;
+}
+
+order = books.sort(compare);
+console.log(order)
